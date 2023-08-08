0bb33dac8aa96682abca3b117b084822e2881858
EbonGit <leben06@outlook.fr>
2023-08-08 10:57:55 +0200

    modify innodeep.py

--- innodeep.py
+++ innodeep.py
@@ -195,7 +195,7 @@ class TumorSeeker:
         st.text(pred_label)
         st.pyplot(fig)
 
-@st.cache_resource
+@st.cache_resource(ttl=3600)
 def load_models():
     seeker = TumorSeeker(model_path, model_inverse_path, model_classification_path, model_ROI_path, model_ROI_type_path)
     return seeker
